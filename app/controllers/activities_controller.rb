9bc5c11163ca785cd5f749d6a5bf7fbacde72c3a
Tony Correia <tonypgh1192@gmail.com>
2013-12-12 21:47:07 -0800

    CSS styling to activity index

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,5 +1,5 @@
 class ActivitiesController < ApplicationController
   def index
-  	@activities = PublicActivity::Activity.order("created_at desc").where(owner_type: "User").having(defined?(trackable) == true)
+  	@activities = PublicActivity::Activity.order("created_at desc").where(owner_type: "User").having(defined?(trackable) == true).first(10)
   end
 end
