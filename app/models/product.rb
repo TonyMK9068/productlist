02359ab291f2013b5b2a141dcf09f5094c3e4bb7
Tony Correia <tonypgh1192@gmail.com>
2013-12-12 23:17:54 -0800

    fix

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,15 +2,13 @@ class Product < ActiveRecord::Base
   attr_accessible :store, :category, :image_url, :name, :price, :product_number, :link
   belongs_to :list, inverse_of: :products
 
-
-
   def receive(message, params)
     message
   end  
 
   def self.top_rated
-    self.select("products.* , COUNT(products.product_number) AS amount").
-      group("products.product_number").
+    self.select("*, COUNT(product_number) AS amount").
+      group("product_number").
       order("amount DESC LIMIT 10")
   end
 end
