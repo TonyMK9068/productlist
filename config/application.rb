294d2bd9c26899c7ae16ad40aacae3616ce86e40
Tony Correia <tonypgh1192@gmail.com>
2013-12-16 21:00:36 -0800

    addded stylesheets to precompile list

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module Giftshare
     config.autoload_paths += Dir["#{config.root}/lib/**/"]
 
     
-    config.assets.precompile += %w(friendships.css searches.css welcome.css custom.css users.css activites.css)
+    config.assets.precompile += %w(friendships.css searches.css welcome.css custom.css users.css activites.css pickadate.js/lib/themes/default.date.css pickadate.js/lib/themes/default)
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
