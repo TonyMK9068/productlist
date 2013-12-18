8660a379997a565779088d299d7e4bc6d2dbdbd4
Tony Correia <tonypgh1192@gmail.com>
2013-12-18 04:03:48 -0800

    fix

--- app/mailers/share_mailer.rb
+++ app/mailers/share_mailer.rb
@@ -2,9 +2,10 @@ class ShareMailer < ActionMailer::Base
   default from: '9324kmdfimjfl234@gmail.com'
 
   def notify(sender, list, recipient)
-    @user, @list, @recipient = sender, list, recipient
+    @user = sender
+    @list = list
+    @recipient = recipient
 
-    mail(to: @recipient,
-      subject: "#{@user.display_user_as("username")} has shared a wish list with you!")
+    mail(to: @recipient, subject: "#{@user.display_user_as("username")} has shared a wish list with you!")
   end
 end
