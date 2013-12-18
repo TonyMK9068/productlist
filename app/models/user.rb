e8756b95d9b4d2cce9920742a9bf0303853574d1
Tony Correia <tonypgh1192@gmail.com>
2013-12-18 01:58:23 -0800

    Edits to user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,19 +13,27 @@ class User < ActiveRecord::Base
   has_many :inverse_friends, :through => :inverse_friendships, :source => :user
   has_many :searches
   
-  validates_uniqueness_of :email
-  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
-  validates_uniqueness_of :username, :allow_nil => true
-  validates_format_of :username, with: /\A([^\W][a-zA-Z]\w+)\Z/i, :allow_nil => true
-  validates_length_of :first_name, minimum: 3, maximum: 30, :allow_nil => true, on: :update
-  validates_format_of :first_name, with: /\A([^\d\W]+)\Z/, :allow_nil => true, on: :update
-  validates_format_of :last_name, with: /\A([^\d\W]+)\Z/, :allow_nil => true, on: :update
-  validates_length_of :last_name, minimum: 3, maximum: 30, :allow_nil => true, on: :update
+
+
+  validates_uniqueness_of :username
+  validates_format_of :username, with: /\A([a-zA-Z0-9]{2,16}[-_]?[a-zA-Z0-9]{2,16})\z/, allow_blank: :true
+  validates_length_of :username, in: 3..18, allow_blank: true
+
+  validates_format_of :first_name, :last_name, with: /\A([^\d\W]+)\z/, allow_blank: :true
+  validates_length_of :first_name, :last_name, in: 3..16, allow_blank: true
 
   after_create :send_confirmation_email
 
   def has_friend?(user)
     self.friends.all.include?(user) ? true : false
+  end
+
+  def display_user_as(title)
+    if title == "username"
+      self.username.present? ? self.username : self.email
+    else
+      self.email
+    end
   end
 
   private
