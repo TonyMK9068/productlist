9733fcc8a3dde694a625cbc771cbf3e076a15eb7
Tony Correia <tonypgh1192@gmail.com>
2014-01-07 05:59:26 -0800

    fix

--- config/initializers/social_share_button.rb
+++ config/initializers/social_share_button.rb
@@ -1,3 +1,3 @@
 SocialShareButton.configure do |config|
-  config.allow_sites = %w(twitter facebook google_plus tumblr email)
+  config.allow_sites = %w(twitter facebook google_plus google_bookmark delicious tumblr)
 end
