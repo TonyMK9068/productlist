a3f7f5afbd931a807659c12e643a148153861ae5
Tony Correia <tonypgh1192@gmail.com>
2014-01-07 19:39:47 -0800

    fix

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -5,35 +5,35 @@ class FriendshipsController < ApplicationController
     @user = current_user
     if @friend.present? && @friend.id != @user.id
       @friendship = @user.friendships.build(:friend_id => @friend.id)
-      authorize! :create, Friendship, message: "Not authorized"
+      authorize! :manage, Friendship, message: "Not authorized"
       if @friendship.save
         @friendship.create_activity(:create, :recipient => @friend, :owner => @user)
         flash[:info] = "Friend added"
         redirect_to user_path(@user)
       else 
         flash[:error] = "An error occured while adding new friend"
-        render 'welcome/index'
+        render "/users/#{@user.id}"
       end
     else
       if @friend.present? && @friend.id == @user.id
         flash[:error] = "Get some real friends!"
-        render 'welcome/index'
+        render "/users/#{@user.id}"
       else
         flash[:error] = "No user with that email"
-        render 'welcome/index'
+        render "/users/#{@user.id}"
       end
     end
   end
 
   def destroy
     @friendship = current_user.friendships.find_by_friend_id(params[:id])
-    authorize! :update, @friendship, message: "Not your friendship to destroy"
+    authorize! :manage, @friendship, message: "Not your friendship to destroy"
     if @friendship.delete
       flash[:info] = "Friend removed successfully"
       redirect_to :back
     else
       flash[:error] ="An error occured while trying to remove the friend"
-      render 'welcome/index'
+      render "/users/#{@user.id}"
     end
   end
 
