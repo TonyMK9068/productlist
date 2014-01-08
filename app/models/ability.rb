e791b5fd7dcf558dd2d406031d51822469b89c6a
Tony Correia <tonypgh1192@gmail.com>
2014-01-07 22:55:12 -0800

    fixed search authoriation

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,11 +7,16 @@ class Ability
       
     if user.created_at
       can :read, User
-      can [:manage], User, user_id: user.id
-      can [:manage], List, user_id: user.id
-      can [:manage], Product, user.lists.all.include?(:list_id)
-      can [:manage], Friendship, user_id: user.id
-      can [:manage], Search, user_id: user.id
+      can :manage, User, id: user.id
+      can :manage, List, user_id: user.id
+      
+      user.lists.all.each do |list|
+        list.products.all.each do |product|
+          can :manage, product
+        end
+      end
+      can :manage, Friendship, user_id: user.id
+      can :manage, Search, user_id: user.id
       can :create, Message, user_id: user.id
     end
 
