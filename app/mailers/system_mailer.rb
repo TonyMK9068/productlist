27dde65bdbb1d2c8e0fa766065dc2a449864fa75
Tony Correia <tonypgh1192@gmail.com>
2014-01-08 16:19:45 -0800

    Fix

--- app/mailers/system_mailer.rb
+++ app/mailers/system_mailer.rb
@@ -2,6 +2,6 @@ class SystemMailer < ActionMailer::Base
   default from: 'notifications@giftshare.io'
 
   def sign_up_notification
-    mail( to: contact.me@anthonyjcorreia.com, subject: "A new user just signed up at #{Time.new.localtime}!")
+    mail( to: 'contact.me@anthonyjcorreia.com', subject: "A new user just signed up at #{Time.new.localtime}!")
   end
 end
