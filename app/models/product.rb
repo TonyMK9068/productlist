d8c5cad640409b5485300eb9f5729fd1a6cb29fb
Tony Correia <tonypgh1192@gmail.com>
2014-01-13 13:01:25 -0800

    fix

--- app/models/product.rb
+++ app/models/product.rb
@@ -9,10 +9,10 @@ class Product < ActiveRecord::Base
   end  
 
   def self.top_ten
-    self.select("COUNT(product_number) AS amount, product_number").
+    self.select('"product_number", COUNT("product_number") AS "amount"').
       group('product_number').
-      having("amount > 1").
-      order("amount DESC").
+      having('"amount" > 1').
+      order('"amount" DESC').
       collect { |product| Product.find_by_product_number(product.product_number) }.
       first(9)
   end
