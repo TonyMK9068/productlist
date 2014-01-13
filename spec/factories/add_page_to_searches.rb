4de0f0fa909d28bd31885d437f0eaf02718dca15
Tony Correia <tonypgh1192@gmail.com>
2014-01-13 01:10:40 -0800

    Updated product thumbnail styling

--- spec/factories/add_page_to_searches.rb
+++
@@ -1,7 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :add_page_to_search, :class => 'AddPageToSearches' do
-    page 1
-  end
-end
